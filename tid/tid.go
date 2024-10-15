4ea53e24e3ce1ab847ad0cc20621121c6e2fa893
MortenSlingsby <morten.slingsby@eviny.no>
2024-10-15 16:26:28 +0200

    go fmt

--- tid/tid.go
+++ tid/tid.go
@@ -2,21 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/urfave/cli/v2"
 	"log"
 	"os"
-  "time"
 	"path/filepath"
-  "fmt"
-  "strconv"
-
+	"strconv"
+	"time"
 )
 
 func db_path() string {
-  homeDir, _ := os.UserHomeDir()
-  db_path := filepath.Join(homeDir, ".tid", "tid.db")
-  return db_path
+	homeDir, _ := os.UserHomeDir()
+	db_path := filepath.Join(homeDir, ".tid", "tid.db")
+	return db_path
 }
 
 func main() {
@@ -24,209 +23,211 @@ func main() {
 		Usage: "App for tidsregistrering",
 		Commands: []*cli.Command{
 			{
-        Name:    "add",
-        Aliases: []string{"a"},
-        Usage:   "Add a new AO to the list",
-        Action: func(cCtx *cli.Context) error {
-          sqliteDatabase, _ := sql.Open("sqlite3", db_path())
-          insertAO := `INSERT INTO AO (code, name) VALUES (?, ?)`
-          defer sqliteDatabase.Close()
-          _ , err := sqliteDatabase.Exec(insertAO, cCtx.Args().First(), cCtx.String("full-name"))
-          if err != nil {
-            panic(err)
-          }
-          fmt.Println("Lagt til ny AO med code:", cCtx.Args().First(), "og beskrivelse: ", cCtx.String("full-name"))
-          return nil
-        },
-        Flags: []cli.Flag{
-            &cli.StringFlag{
-                Name:  "full-name",
-                Usage:  "Full name of AO",
-                Aliases: []string{"f"},
-                Value: "",
-                Required: true,
-            },
-        },
-        Before: func(cCtx *cli.Context) error {
-          if cCtx.Args().Len() != 1 {
-            return fmt.Errorf("Expecting exactly 1 argument")
-          }
-          return nil
-        },
-      },
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "Add a new AO to the list",
+				Action: func(cCtx *cli.Context) error {
+					sqliteDatabase, _ := sql.Open("sqlite3", db_path())
+					insertAO := `INSERT INTO AO (code, name) VALUES (?, ?)`
+					defer sqliteDatabase.Close()
+					_, err := sqliteDatabase.Exec(insertAO, cCtx.Args().First(), cCtx.String("full-name"))
+					if err != nil {
+						panic(err)
+					}
+					fmt.Println("Lagt til ny AO med code:", cCtx.Args().First(), "og beskrivelse: ", cCtx.String("full-name"))
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "full-name",
+						Usage:    "Full name of AO",
+						Aliases:  []string{"f"},
+						Value:    "",
+						Required: true,
+					},
+				},
+				Before: func(cCtx *cli.Context) error {
+					if cCtx.Args().Len() != 1 {
+						return fmt.Errorf("Expecting exactly 1 argument")
+					}
+					return nil
+				},
+			},
 			{
-        Name:    "start",
-        Aliases: []string{"s"},
-        Usage:   "Start time for an AO",
-        Action: func(cCtx *cli.Context) error {
-          sqliteDatabase, _ := sql.Open("sqlite3", db_path())
-          now := time.Now().Format("2006-01-02 15:04:05")
-          updateQuery := `
+				Name:    "start",
+				Aliases: []string{"s"},
+				Usage:   "Start time for an AO",
+				Action: func(cCtx *cli.Context) error {
+					sqliteDatabase, _ := sql.Open("sqlite3", db_path())
+					now := time.Now().Format("2006-01-02 15:04:05")
+					updateQuery := `
             UPDATE log
             SET active = 0, end_time = ?, duration = strftime('%s', ?) - strftime('%s', start_time)
             WHERE active = 1;
           `
-          _, err := sqliteDatabase.Exec(updateQuery, now, now)
-          if err != nil {
-              log.Fatal(err)
-          }
+					_, err := sqliteDatabase.Exec(updateQuery, now, now)
+					if err != nil {
+						log.Fatal(err)
+					}
 
-          insertAO := `INSERT INTO log (code, start_time, end_time, duration, active) VALUES (?, ?, ?, ?, ?)`
-          exists, err := valueExists(sqliteDatabase, cCtx.Args().First())
-          if !exists {
-            log.Fatal("Code does not exist")
-          }
-          if err != nil {
-            log.Fatal(err)
-          }
-          defer sqliteDatabase.Close()
-          _ , err = sqliteDatabase.Exec(insertAO, cCtx.Args().First(), now, "", "", 1)
-          if err != nil {
-            panic(err)
-          }
-          fmt.Println("Starting log for:", cCtx.Args().First())
-          return nil
-        },
-        Before: func(cCtx *cli.Context) error {
-          if cCtx.Args().Len() != 1 {
-            return fmt.Errorf("Expecting exactly 1 argument")
-          }
-          return nil
-        },
-      },
+					insertAO := `INSERT INTO log (code, start_time, end_time, duration, active) VALUES (?, ?, ?, ?, ?)`
+					exists, err := valueExists(sqliteDatabase, cCtx.Args().First())
+					if !exists {
+						log.Fatal("Code does not exist")
+					}
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer sqliteDatabase.Close()
+					_, err = sqliteDatabase.Exec(insertAO, cCtx.Args().First(), now, "", "", 1)
+					if err != nil {
+						panic(err)
+					}
+					fmt.Println("Starting log for:", cCtx.Args().First())
+					return nil
+				},
+				Before: func(cCtx *cli.Context) error {
+					if cCtx.Args().Len() != 1 {
+						return fmt.Errorf("Expecting exactly 1 argument")
+					}
+					return nil
+				},
+			},
 			{
-        Name:    "stop",
-        Usage:   "Stop log for today",
-        Action: func(cCtx *cli.Context) error {
-          sqliteDatabase, _ := sql.Open("sqlite3", db_path())
-          now := time.Now().Format("2006-01-02 15:04:05")
-          updateQuery := `
+				Name:  "stop",
+				Usage: "Stop log for today",
+				Action: func(cCtx *cli.Context) error {
+					sqliteDatabase, _ := sql.Open("sqlite3", db_path())
+					now := time.Now().Format("2006-01-02 15:04:05")
+					updateQuery := `
             UPDATE log
             SET active = 0, end_time = ?, duration = strftime('%s', ?) - strftime('%s', start_time)
 
             WHERE active = 1;
           `
-          _, err := sqliteDatabase.Exec(updateQuery, now, now)
-          if err != nil {
-              log.Fatal(err)
-          }
-          defer sqliteDatabase.Close()
-          fmt.Println("End log for today")
-          return nil
-        },
-      },
+					_, err := sqliteDatabase.Exec(updateQuery, now, now)
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer sqliteDatabase.Close()
+					fmt.Println("End log for today")
+					return nil
+				},
+			},
 			{
-        Name:    "view",
-        Usage:   "Show aggregated data",
-        Action: func(cCtx *cli.Context) error {
-          var week int
-          if cCtx.NArg() > 0 {
-            i, err := strconv.Atoi(cCtx.Args().Get(0))
-            if err != nil {
-                panic(err)
-            }
-            week = -i*7+1
-          } else {
-            week = -6
-          }
-          sqliteDatabase, _ := sql.Open("sqlite3", db_path())
-          createTable(sqliteDatabase, week)
-          defer sqliteDatabase.Close()
+				Name:  "view",
+				Usage: "Show aggregated data",
+				Action: func(cCtx *cli.Context) error {
+					var week int
+					if cCtx.NArg() > 0 {
+						i, err := strconv.Atoi(cCtx.Args().Get(0))
+						if err != nil {
+							panic(err)
+						}
+						week = -i*7 + 1
+					} else {
+						week = -6
+					}
+					sqliteDatabase, _ := sql.Open("sqlite3", db_path())
+					createTable(sqliteDatabase, week)
+					defer sqliteDatabase.Close()
 
-          return nil
-        },
-      },
+					return nil
+				},
+			},
 			{
-        Name:    "log",
-        Usage:   "Show todays log so far",
-        Action: func(cCtx *cli.Context) error {
-          sqliteDatabase, _ := sql.Open("sqlite3", db_path())
-          showLog(sqliteDatabase)
-          defer sqliteDatabase.Close()
-          
-          todayTotalOut(sqliteDatabase)
-          return nil
-        },
-      },
+				Name:  "log",
+				Usage: "Show todays log so far",
+				Action: func(cCtx *cli.Context) error {
+					sqliteDatabase, _ := sql.Open("sqlite3", db_path())
+					showLog(sqliteDatabase)
+					defer sqliteDatabase.Close()
+
+					todayTotalOut(sqliteDatabase)
+					return nil
+				},
+			},
 			{
-        Name:    "list",
-        Usage:   "List all AOs in the table",
-        Action: func(cCtx *cli.Context) error {
-          sqliteDatabase, _ := sql.Open("sqlite3", db_path())
-          showAO(sqliteDatabase)
-          defer sqliteDatabase.Close()
+				Name:  "list",
+				Usage: "List all AOs in the table",
+				Action: func(cCtx *cli.Context) error {
+					sqliteDatabase, _ := sql.Open("sqlite3", db_path())
+					showAO(sqliteDatabase)
+					defer sqliteDatabase.Close()
 
-          return nil
-        },
-      },
+					return nil
+				},
+			},
 			{
-        Name:    "drop",
-        Usage:   "Drop log or AO entry",
-        Action: func(cCtx *cli.Context) error {
-          sqliteDatabase, _ := sql.Open("sqlite3", db_path())
-          defer sqliteDatabase.Close()
-          
-          if cCtx.String("AO") != "" {
-            dropAO(sqliteDatabase, cCtx.String("AO"))
-            fmt.Println("Dropped AO with tag:",cCtx.String("AO"))
-            return nil
-          } 
+				Name:  "drop",
+				Usage: "Drop log or AO entry",
+				Action: func(cCtx *cli.Context) error {
+					sqliteDatabase, _ := sql.Open("sqlite3", db_path())
+					defer sqliteDatabase.Close()
 
-          if cCtx.String("log") != "" {
-            id, err := strconv.Atoi(cCtx.String("log"))
-            if err != nil {
-              panic(err)
-            }
+					if cCtx.String("AO") != "" {
+						dropAO(sqliteDatabase, cCtx.String("AO"))
+						fmt.Println("Dropped AO with tag:", cCtx.String("AO"))
+						return nil
+					}
 
-            dropLog(sqliteDatabase, id)
-            fmt.Println("Dropped log with ID:", cCtx.String("log"))
-            return nil
-          } 
+					if cCtx.String("log") != "" {
+						id, err := strconv.Atoi(cCtx.String("log"))
+						if err != nil {
+							panic(err)
+						}
 
-          fmt.Println("You forgot to provide a flag and log id or AO tag")
+						dropLog(sqliteDatabase, id)
+						fmt.Println("Dropped log with ID:", cCtx.String("log"))
+						return nil
+					}
 
-          return nil
-        },
-        Flags: []cli.Flag{
-            &cli.StringFlag{
-                Name:  "AO",
-                Usage:  "Tag of AO to drop",
-                Value: "",
-                Required: false,
-            },
-            &cli.StringFlag{
-                Name:  "log",
-                Usage:  "ID of to drop",
-                Value: "",
-                Required: false,
-            },
-        },
-      },
+					fmt.Println("You forgot to provide a flag and log id or AO tag")
+
+					return nil
+				},
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "AO",
+						Usage:    "Tag of AO to drop",
+						Value:    "",
+						Required: false,
+					},
+					&cli.StringFlag{
+						Name:     "log",
+						Usage:    "ID of to drop",
+						Value:    "",
+						Required: false,
+					},
+				},
+			},
 			{
-        Name:    "fix",
-        Usage:   "Add custom duration to an AO",
-        Action: func(cCtx *cli.Context) error {
-          sqliteDatabase, _ := sql.Open("sqlite3", db_path())
-          duration, err := strconv.Atoi(cCtx.Args().Get(1))
-          duration_sec := duration * 60
-          now := time.Now().Format("2006-01-02 15:04:05")
-          if err != nil { panic(err) }
-          insertFix := `INSERT INTO log (code, start_time, end_time, duration, active) VALUES (?, ?, ?, ?, ?)`
-          defer sqliteDatabase.Close()
-          _ , err = sqliteDatabase.Exec(insertFix, cCtx.Args().First(), now, "fix", duration_sec, 0)
-          if err != nil {
-            panic(err)
-          }
-          fmt.Println("Added custom duration for", cCtx.Args().First(), "of", duration, "minutes")
-          return nil
-        },
-        Before: func(cCtx *cli.Context) error {
-          if cCtx.Args().Len() != 2 {
-            return fmt.Errorf("Expecting exactly 2 arguments: <tag> <duration_minutes>")
-          }
-          return nil
-        },
-      },
+				Name:  "fix",
+				Usage: "Add custom duration to an AO",
+				Action: func(cCtx *cli.Context) error {
+					sqliteDatabase, _ := sql.Open("sqlite3", db_path())
+					duration, err := strconv.Atoi(cCtx.Args().Get(1))
+					duration_sec := duration * 60
+					now := time.Now().Format("2006-01-02 15:04:05")
+					if err != nil {
+						panic(err)
+					}
+					insertFix := `INSERT INTO log (code, start_time, end_time, duration, active) VALUES (?, ?, ?, ?, ?)`
+					defer sqliteDatabase.Close()
+					_, err = sqliteDatabase.Exec(insertFix, cCtx.Args().First(), now, "fix", duration_sec, 0)
+					if err != nil {
+						panic(err)
+					}
+					fmt.Println("Added custom duration for", cCtx.Args().First(), "of", duration, "minutes")
+					return nil
+				},
+				Before: func(cCtx *cli.Context) error {
+					if cCtx.Args().Len() != 2 {
+						return fmt.Errorf("Expecting exactly 2 arguments: <tag> <duration_minutes>")
+					}
+					return nil
+				},
+			},
 		},
 	}
 	if fileExists(db_path()) {
@@ -280,13 +281,13 @@ func initLog(db *sql.DB) {
 }
 
 func valueExists(db *sql.DB, value string) (bool, error) {
-    var exists bool
+	var exists bool
 
-    query := `SELECT EXISTS(SELECT 1 FROM AO WHERE code = ? LIMIT 1)`
-    err := db.QueryRow(query, value).Scan(&exists)
-    if err != nil {
-        return false, err
-    }
+	query := `SELECT EXISTS(SELECT 1 FROM AO WHERE code = ? LIMIT 1)`
+	err := db.QueryRow(query, value).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
 
-    return exists, nil
+	return exists, nil
 }
--- tid/view.go
+++ tid/view.go
@@ -4,141 +4,141 @@ import (
 	"database/sql"
 	"fmt"
 
-	_ "github.com/mattn/go-sqlite3"
-	 "time"
 	"github.com/jedib0t/go-pretty/v6/table"
+	_ "github.com/mattn/go-sqlite3"
+	"time"
 )
 
 type AO struct {
-  tag string
-  description string
+	tag         string
+	description string
 }
 
 type Log struct {
-  id int
-  tag string
-  description string
-  startTime string
-  endTime string
-  duration string
-  active string
+	id          int
+	tag         string
+	description string
+	startTime   string
+	endTime     string
+	duration    string
+	active      string
 }
 
 func createTable(db *sql.DB, week int) {
-  codes := get_codes(db, week)
-  t := table.NewWriter()
-  for _, code := range codes {
-    row := calcRow(db, code, week)
-    t.AppendRow(row)
-  }
-  t.AppendHeader(calcHeader(week))
-  fmt.Println(t.Render())
+	codes := get_codes(db, week)
+	t := table.NewWriter()
+	for _, code := range codes {
+		row := calcRow(db, code, week)
+		t.AppendRow(row)
+	}
+	t.AppendHeader(calcHeader(week))
+	fmt.Println(t.Render())
 }
 
 func get_first_day(week int) time.Time {
-  now := time.Now()
-  weekDay := int(now.Weekday())
-  return now.AddDate(0, 0, -weekDay+week)
+	now := time.Now()
+	weekDay := int(now.Weekday())
+	return now.AddDate(0, 0, -weekDay+week)
 }
 
 func secondString(seconds int) string {
-    hours := seconds / 3600
-    minutes := (seconds % 3600) / 60
-    return fmt.Sprintf("%02d:%02d", hours, minutes)
+	hours := seconds / 3600
+	minutes := (seconds % 3600) / 60
+	return fmt.Sprintf("%02d:%02d", hours, minutes)
 }
 
 func calcHeader(week int) []interface{} {
-  row := make([]interface{}, 8)
-  row[0] = "AO"
-  date := get_first_day(week)
-  for i := 0; i < 7; i++ {
-    row[i+1] = fmt.Sprintf("%s\n%s", date.Format("02.01"), date.Weekday().String())
-    date = date.AddDate(0,0,1)
-  }
-  return row
+	row := make([]interface{}, 8)
+	row[0] = "AO"
+	date := get_first_day(week)
+	for i := 0; i < 7; i++ {
+		row[i+1] = fmt.Sprintf("%s\n%s", date.Format("02.01"), date.Weekday().String())
+		date = date.AddDate(0, 0, 1)
+	}
+	return row
 }
 
 func calcRow(db *sql.DB, code string, week int) []interface{} {
-  row := make([]interface{}, 8)
-  row[0] = fullName(db, code)
-  date := get_first_day(week)
-  for i := 0; i < 7; i++ {
-    row[i+1] = secondString(calcVal(db, code, date.Format("2006-01-02")))
-    date = date.AddDate(0,0,1)
-  }
-  return row
+	row := make([]interface{}, 8)
+	row[0] = fullName(db, code)
+	date := get_first_day(week)
+	for i := 0; i < 7; i++ {
+		row[i+1] = secondString(calcVal(db, code, date.Format("2006-01-02")))
+		date = date.AddDate(0, 0, 1)
+	}
+	return row
 }
 
 func calcVal(db *sql.DB, code string, date string) int {
-  query := "SELECT sum(duration) FROM log WHERE code = ? AND DATE(start_time) = ?"
+	query := "SELECT sum(duration) FROM log WHERE code = ? AND DATE(start_time) = ?"
 
-  var result sql.NullInt64
-  err := db.QueryRow(query, code, date).Scan(&result)
-  if err != nil {
-      if err == sql.ErrNoRows {
-          fmt.Println("No rows were returned!")
-          return 0
-      } else {
-          panic(err)
-      }
-  }
-  if result.Valid {
-    return int(result.Int64)
-  }
-  return 0
+	var result sql.NullInt64
+	err := db.QueryRow(query, code, date).Scan(&result)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("No rows were returned!")
+			return 0
+		} else {
+			panic(err)
+		}
+	}
+	if result.Valid {
+		return int(result.Int64)
+	}
+	return 0
 }
 
 func fullName(db *sql.DB, code string) string {
-  query := "SELECT name FROM AO WHERE code = ?"
-  var result string
-  err := db.QueryRow(query, code).Scan(&result)
-  if err != nil {
-    panic(err)
-  }
-  return result
+	query := "SELECT name FROM AO WHERE code = ?"
+	var result string
+	err := db.QueryRow(query, code).Scan(&result)
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
 
 func get_codes(db *sql.DB, week int) []string {
-  var result []string
-  first_date := get_first_day(week)
-  last_date := first_date.AddDate(0,0,7)
-  query := "SELECT DISTINCT code FROM log WHERE start_time > ? and end_time < ?"
-  rows, err := db.Query(query, first_date.Format("2006-01-02"), last_date.Format("2006-01-02"))
-  if err != nil {
-      panic(err)
-  }
-  defer rows.Close()
+	var result []string
+	first_date := get_first_day(week)
+	last_date := first_date.AddDate(0, 0, 7)
+	query := "SELECT DISTINCT code FROM log WHERE start_time > ? and end_time < ?"
+	rows, err := db.Query(query, first_date.Format("2006-01-02"), last_date.Format("2006-01-02"))
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
 
-  for rows.Next() {
-      var value string
-      if err := rows.Scan(&value); err != nil {
-          panic(err)
-      }
-      result = append(result, value)
-  }
-  return result
+	for rows.Next() {
+		var value string
+		if err := rows.Scan(&value); err != nil {
+			panic(err)
+		}
+		result = append(result, value)
+	}
+	return result
 }
 
 func showAO(db *sql.DB) {
-  rows, err := db.Query("SELECT * FROM AO")
-  if err != nil {
-    panic(err)
-  }
-  t := table.NewWriter()
-  for rows.Next() {
-    var aO AO
-    if err := rows.Scan(&aO.tag, &aO.description); err != nil {
-        panic(err)
-    }
-    t.AppendRow(table.Row{aO.tag, aO.description})
-  }
-  t.AppendHeader(table.Row{"Tag", "Description"})
-  fmt.Println(t.Render())
+	rows, err := db.Query("SELECT * FROM AO")
+	if err != nil {
+		panic(err)
+	}
+	t := table.NewWriter()
+	for rows.Next() {
+		var aO AO
+		if err := rows.Scan(&aO.tag, &aO.description); err != nil {
+			panic(err)
+		}
+		t.AppendRow(table.Row{aO.tag, aO.description})
+	}
+	t.AppendHeader(table.Row{"Tag", "Description"})
+	fmt.Println(t.Render())
 }
 
 func showLog(db *sql.DB) {
-  now := time.Now().Format("2006-01-02")
-  rows, err := db.Query(`
+	now := time.Now().Format("2006-01-02")
+	rows, err := db.Query(`
     select
       l.id,
       l.code,
@@ -156,67 +156,67 @@ func showLog(db *sql.DB) {
     inner join AO as a ON l.code = a.code
     WHERE start_time > ?
   `, now)
-  if err != nil {
-    panic(err)
-  }
-  t := table.NewWriter()
-  for rows.Next() {
-    var log Log
-    if err := rows.Scan(&log.id, &log.tag, &log.description, &log.startTime, &log.endTime, &log.duration, &log.active); err != nil {
-        panic(err)
-    }
-    t.AppendRow(table.Row{log.id, log.tag, log.description, log.startTime, log.endTime, log.duration, log.active})
-  }
-  t.AppendHeader(table.Row{"ID", "Tag", "Description", "Start Time", "End time", "Duration", "Active"})
-  fmt.Println(t.Render())
+	if err != nil {
+		panic(err)
+	}
+	t := table.NewWriter()
+	for rows.Next() {
+		var log Log
+		if err := rows.Scan(&log.id, &log.tag, &log.description, &log.startTime, &log.endTime, &log.duration, &log.active); err != nil {
+			panic(err)
+		}
+		t.AppendRow(table.Row{log.id, log.tag, log.description, log.startTime, log.endTime, log.duration, log.active})
+	}
+	t.AppendHeader(table.Row{"ID", "Tag", "Description", "Start Time", "End time", "Duration", "Active"})
+	fmt.Println(t.Render())
 }
 
 func todayTotalOut(db *sql.DB) {
-  total := secondString(todayTotalNonActive(db) + todayTotalActive(db))
-  expected := secondString(todayTotal(db))
-  out := fmt.Sprintf("You have logged %s out of a possible %s today", total, expected)
-  fmt.Println(out)
+	total := secondString(todayTotalNonActive(db) + todayTotalActive(db))
+	expected := secondString(todayTotal(db))
+	out := fmt.Sprintf("You have logged %s out of a possible %s today", total, expected)
+	fmt.Println(out)
 }
 
 func todayTotalNonActive(db *sql.DB) int {
-  var sum int
-  now := time.Now().Format("2006-01-02")
-  err := db.QueryRow(`
+	var sum int
+	now := time.Now().Format("2006-01-02")
+	err := db.QueryRow(`
     select
       sum(duration)
     FROM log
     WHERE start_time > ? AND active = 0
   `, now).Scan(&sum)
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  return sum
+	return sum
 }
 
 func todayTotalActive(db *sql.DB) int {
-  var sum sql.NullInt64
-  now := time.Now().Format("2006-01-02")
-  err := db.QueryRow(`
+	var sum sql.NullInt64
+	now := time.Now().Format("2006-01-02")
+	err := db.QueryRow(`
     select
         sum(strftime('%s', 'now', 'utc') - strftime('%s', start_time, 'utc'))
     FROM log
     WHERE start_time > ? AND active = 1
   `, now).Scan(&sum)
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  if sum.Valid {
-    return int(sum.Int64)
-  }
-  return 0
+	if sum.Valid {
+		return int(sum.Int64)
+	}
+	return 0
 }
 
 func todayTotal(db *sql.DB) int {
-  var sum sql.NullInt64
-  now := time.Now().Format("2006-01-02")
-  err := db.QueryRow(`
+	var sum sql.NullInt64
+	now := time.Now().Format("2006-01-02")
+	err := db.QueryRow(`
     select
         strftime('%s', 'now', 'utc') - strftime('%s', start_time, 'utc')
     FROM log
@@ -224,32 +224,31 @@ func todayTotal(db *sql.DB) int {
     ORDER BY start_time ASC
     LIMIT 1
   `, now).Scan(&sum)
-  if err != nil {
-    panic(err)
-  }
-  if sum.Valid {
-    return int(sum.Int64)
-  }
-  return 0
+	if err != nil {
+		panic(err)
+	}
+	if sum.Valid {
+		return int(sum.Int64)
+	}
+	return 0
 }
 
 func dropLog(db *sql.DB, id int) string {
-  query := "DELETE FROM log WHERE id = ?"
-  var result string
-  _, err := db.Exec(query, id)
-  if err != nil {
-    panic(err)
-  }
-  return result
+	query := "DELETE FROM log WHERE id = ?"
+	var result string
+	_, err := db.Exec(query, id)
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
 
 func dropAO(db *sql.DB, id string) string {
-  query := "DELETE FROM AO WHERE code = ?"
-  var result string
-  _, err := db.Exec(query, id)
-  if err != nil {
-    panic(err)
-  }
-  return result
+	query := "DELETE FROM AO WHERE code = ?"
+	var result string
+	_, err := db.Exec(query, id)
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
-
